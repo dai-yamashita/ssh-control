713902fd261ce9ebed492c244b54a08f737c2aed
V Sreekanth <sreeix@gmail.com>
2013-04-24 17:59:37 +0530

    Adding winston as a logger

--- example/taskless.js
+++ example/taskless.js
@@ -11,3 +11,12 @@ for (i = 0, l = controllers.length; i < l; i += 1) {
     controller = controllers[i];
     controller.ssh('date');
 }
+
+
+
+
+var control = require('./index'),
+    controller = Object.create(control.controller, {address: {value:"127.0.0.1"}, user:{value: 'v'}});
+
+controller.ssh("ls -l")
+
